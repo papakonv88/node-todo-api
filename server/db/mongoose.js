764d821d6418f6403acdee73a7317e5ebdc16169
Vasilis <papakonv7@gmail.com>
2018-12-26 21:36:24 +0200

    Add delete route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-
+ 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp',{useNewUrlParser: true});
 
 module.export = {mongoose};
 
-
-/*process.ENV.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'*/
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
+
 
 var app = express();
 
@@ -50,6 +51,24 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.delete('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    
+    Todo.findByIdAndRemove(id).then((todo)=>{
+        if (!todo) {
+        return res.status(404).send();
+        }
+        
+        res.send({todo: todo});
+    }).catch((e)=> {
+        res.status(400).send();
+    });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
