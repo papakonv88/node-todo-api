8faf60b5fec35ece7013ea957e024d114d95ad0a
Vasilis <papakonv7@gmail.com>
2019-01-12 16:30:42 +0200

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,18 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user=this;
+    
+    return this.update({
+        $pull: {
+            tokens: {
+              token: token
+            }
+        }
+    });
+};
+
 
 UserSchema.statics.findByToken = function (token) {
     
